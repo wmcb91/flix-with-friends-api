fb91c57e990eaffc5827fb026d4a2f534afaa272
wmcb91 <wmcb91@gmail.com>
2016-11-10 10:09:58 -0500

    Small change

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -1,6 +1,6 @@
 class Screening < ActiveRecord::Base
   belongs_to :user, inverse_of: :screenings
   belongs_to :movie, inverse_of: :screenings
-  validates :user_rating, presence: true
+  validates :user_rating, presence: true, numericality: { only_integer: true }
   validates :movie_id, uniqueness: { scope: :user_id }
 end
